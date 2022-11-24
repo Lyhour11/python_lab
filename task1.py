0cce63dfdf6024694d8b388736b5fe46f83107f2
Lyhour11 <tithlyhour8088@gmail.com>
2022-11-24 16:14:54 +0300

    lab 3

---
+++ task1.py
@@ -0,0 +1,7 @@
+def sum10(lst):
+    total = 0
+    for num in lst:
+        total = total + num 
+    return total 
+
+print(sum10([1,2,3,4,5,6,7,8,9,10]))   
---
+++ task4.py
@@ -0,0 +1,15 @@
+n = int(input("enter a number: "))
+
+
+for i in range(1,n+1):
+
+    for j in range(n-i,0,-1):
+        print(" ",end="")
+
+    for k in range(1,i+1):
+        print(k,end='')
+
+    for t in range(i-1,0,-1):
+        print(t,end='')
+
+    print()
